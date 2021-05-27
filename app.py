96bc0493612c8c36da17a91a0dd48c6fd875b236
jjaen0823 <poungki0823@gmail.com>
2021-05-27 13:03:37 +0900

    modified app

--- app.py
+++ app.py
@@ -1,8 +1,7 @@
 import os
-from flask import Flask, redirect, render_template, url_for, request, flash
-from flask_sqlalchemy import SQLAlchemy
+from controller import Controller
+from flask import Flask, redirect, url_for, request
 from flask_wtf.csrf import CSRFProtect
-from models import db
 
 # Flask
 app = Flask(__name__)
@@ -15,25 +14,44 @@ def index():
     '''
     return redirect(url_for('restaurants'))
 
+@app.route("/restaurants")
+def restaurants():
+    '''
+    Show alls my_restaurants
+    '''
+    cont = Controller()
+    return cont.restaurants_list_controller()
+
+@app.route("/register_restaurant", methods=('GET', 'POST'))
+def register_restaurant():
+    cont = Controller()
+    return cont.register_restaurant_controller(request)
+
+
+@app.route("/update_restaurant/<id>", methods=('GET', 'POST'))
+def update_restaurant(id):
+    cont = Controller()
+    return cont.update_restaurant_controller(request, id)
+
+
+@app.route("/restaurants/delete", methods=('POST',))
+def delete_restaurant():
+    cont = Controller()
+    return cont.delete_restaurant_controller(request)
 
 
 if __name__ == "__main__":
     basedir = os.path.abspath(os.path.dirname(__file__))
-    dbfile = os.path.join(basedir, 'db.sqlite')
 
-    app.config["SQLALCHEMY_DATABASE_URI"] = "mysql+pymysql://root:1234@localhost:3306/flask_test"
-    app.config['SQLALCHEMY_COMMIT_ON_TEARDOWN'] = True  # 사용자에게 원하는 정보를 전달할 때 나오는게 TEARDOWN, 그럴 때마다 COMMIT(실제로 DB에 반영)
-    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False  #
-    app.config['SECRET_KEY'] = 'my secret' 
-
+    # app.config['SQLALCHEMY_COMMIT_ON_TEARDOWN'] = True  # 사용자에게 원하는 정보를 전달할 때 나오는게 TEARDOWN, 그럴 때마다 COMMIT(실제로 DB에 반영)
+    # app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False  #
 
     # csrf
     csrf = CSRFProtect(app)
     csrf.init_app(app)
 
-
-    db.init_app(app)
-    db.app = app
-    db.create_all()
+    # db.init_app(app)
+    # db.app = app
+    # db.create_all()
 
     app.run(host="127.0.0.1", port=5000, debug=True)
