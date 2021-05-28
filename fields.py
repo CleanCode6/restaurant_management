42bfe71d2d3190a247f9a4953796d0cfe755363d
jjaen0823 <poungki0823@gmail.com>
2021-05-28 12:23:13 +0900

    JsonField for form

---
+++ fields.py
@@ -0,0 +1,25 @@
+import json
+from wtforms import fields
+
+
+# JSON field for WTForms that converts between the form string data and a dictionary representation, with validation
+class JSONField(fields.StringField):
+    def _value(self):
+        return json.dumps(self.data) if self.data else ''
+
+    def process_formdata(self, valuelist):
+        if valuelist:
+            try:
+                self.data = json.loads(valuelist[0])
+            except ValueError:
+                raise ValueError('This field contains invalid JSON')
+        else:
+            self.data = None
+
+    def pre_validate(self, form):
+        super().pre_validate(form)
+        if self.data:
+            try:
+                json.dumps(self.data)
+            except TypeError:
+                raise ValueError('This field contains invalid JSON')
