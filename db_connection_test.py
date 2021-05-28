46e6808f648c6b009ed1e0c53d2f4f2e4fd1bde6
jjaen0823 <poungki0823@gmail.com>
2021-05-28 12:22:24 +0900

    db_connection test

---
+++ db_connection_test.py
@@ -0,0 +1,24 @@
+import unittest
+from .db_connection import DBConnection
+
+
+class DBConnectionTest(unittest.TestCase):
+	def test_get_restaurant_list(self):
+		db = DBConnection()
+		db.connect_to_db()
+		result = db.get_restaurants()
+		print(result)
+
+	def test_get_restaurants(self):
+		db = DBConnection()
+		db.connect_to_db()
+		rest_id = 1
+		result = db.get_restaurant(rest_id)
+		print(result)
+
+	# def test_register_restaurant(self):
+	# 	db = DBConnection()
+	# 	db.connect_to_db()
+
+if __name__ == '__main__':
+	unittest.main()
