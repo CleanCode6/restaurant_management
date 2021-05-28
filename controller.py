0f250df62eeb4ffec77cca7ea631453594be869d
jjaen0823 <poungki0823@gmail.com>
2021-05-28 12:13:54 +0900

    exclude csrf_token from restaurent query

--- controller.py
+++ controller.py
@@ -12,10 +12,11 @@ class Controller:
 	def __init__(self):
 		self.db = DBConnection()
 		self.page_mk = PageMaker()
-		self.db.connect_to_db()
+		# self.db.connect_to_db()
 
 	def restaurants_list_controller(self):
 		rests_query = self.db.get_restaurants()
+		print(rests_query)
 		self.db.close_connection()
 		return self.page_mk.make_restautants_list_page(rests_query)
 		
@@ -23,8 +24,10 @@ class Controller:
 		form = RestaurantForm()
 		if form.validate_on_submit():
 			rest_query = request.form.to_dict(flat=False)
-			self.db.register_restaurant(rest_query)  
-			self.db.close_connection()
+			del_csrf = rest_query
+			del(del_csrf['csrf_token'])
+			self.db.register_restaurant(del_csrf)
+			
 			try:
 				flash('Restaurant registered correctly', 'success')
 				# 설계에는 없음..! 일단,,
@@ -32,33 +35,36 @@ class Controller:
 			except:
 				self.db.rollback()
 				flash('Error restaurant registeration.', 'danger')
+		
 		# get
 		return self.page_mk.make_resgister_page(form)
 
-	def update_restaurant_controller(self, request, id):
-		rest = self.db.get_restaurant(id)
+	def update_restaurant_controller(self, request, restaurant_id):
+		rest = self.db.get_restaurant(restaurant_id)
+		print('controller: \n',rest)
+		# print(type(rest))
 		form = RestaurantForm(obj=rest)
 
 		if form.validate_on_submit():
-			rest_query = request.form.to_dict(flat=False)
-			self.db.update_restaurant(rest_query) 
-			self.db.close_connection()
+			rest_query = request.form.to_dict(flat=False) #  rest_query = request.form
+			del_csrf = rest_query
+			del(del_csrf['csrf_token'])
+			self.db.update_restaurant(del_csrf, restaurant_id)
 			
 			try:
-				flash('Restaurant registered correctly', 'success')
+				flash('Restaurant updated correctly', 'success')
 				
 			except:
 				self.db.rollback()
-				flash('Error restaurant registeration.', 'danger')
+				flash('Error restaurant update.', 'danger')
 		
 		return self.page_mk.make_update_page(form)
 		
 	
 	def delete_restaurant_controller(self, request):
 		try:
-			id=request.form['id']  # str
-			self.db.delete_restaurant(id) 
-			self.db.close_connection()
+			restaurant_id=request.form['restaurant_id']  # str
+			self.db.delete_restaurant(restaurant_id) 
 			flash('Delete successfully.', 'danger')
 		except:
 			self.db.rollback()
